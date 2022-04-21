352db4ce21f5086ec32ad52529694dce13f29450
Aasheesh Mahammad <54064611+AasheeshMahammad@users.noreply.github.com>
2022-04-21 20:31:21 +0530

    Image resizes according to window size

--- gui.py
+++ gui.py
@@ -4,9 +4,10 @@ import os
 from PIL import Image, ImageTk
 
 class Restore:
-    def __init__(self, view):
+    def __init__(self, view,size):
         self.view = view
         self.filepath = None
+        self.size = size
 
     def selectImage(self):
         file = filedialog.askopenfilename(
@@ -14,6 +15,7 @@ class Restore:
             title = "Select Image",
             filetype = (("JPG File","*.jpg"),("PNG File","*.png"),("All  Files","*.txt")))
         img = Image.open(file)
+        img = img.resize(self.size)
         img = ImageTk.PhotoImage(img)
         self.view.configure(image = img)
         self.view.image = img
@@ -26,11 +28,12 @@ class Restore:
 main =tk.Tk()
 frame = tk.Frame(main)
 main.title("Image Restoration")
-main.geometry("450x450")
+size = (400,300)
+main.geometry(f"{size[0]}x{size[1]}")
 frame.pack(side=tk.BOTTOM,padx = 10, pady = 15)
 view = tk.Label()
 view.pack()
-obj = Restore(view)
+obj = Restore(view,size)
 btn1 = tk.Button(frame, text = "Select Image to Restore", command=obj.selectImage)
 btn1.pack(side=tk.LEFT,padx=10)
 
