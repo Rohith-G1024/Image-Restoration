dd835d50ae88bf87236faf37bd15b1a52b1dd7cb
Aasheesh Mahammad <54064611+AasheeshMahammad@users.noreply.github.com>
2022-04-21 20:01:41 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,18 +3,24 @@ from tkinter import filedialog
 import os
 from PIL import Image, ImageTk
 
-def selectImage():
-    file = filedialog.askopenfilename(
-        initialdir = os.getcwd(),
-        title = "Select Image",
-        filetype = (("JPG File","*.jpg"),("PNG File","*.png"),("All  Files","*.txt")))
-    img = Image.open(file)
-    img = ImageTk.PhotoImage(img)
-    view.configure(image = img)
-    view.image = img
+class Restore:
+    def __init__(self, view):
+        self.view = view
+        self.filepath = None
 
-def fixImage():
-    pass
+    def selectImage(self):
+        file = filedialog.askopenfilename(
+            initialdir = os.getcwd(),
+            title = "Select Image",
+            filetype = (("JPG File","*.jpg"),("PNG File","*.png"),("All  Files","*.txt")))
+        img = Image.open(file)
+        img = ImageTk.PhotoImage(img)
+        self.view.configure(image = img)
+        self.view.image = img
+        self.filepath = file
+
+    def fixImage(self):
+        pass
 
 
 main =tk.Tk()
@@ -24,11 +30,11 @@ main.geometry("450x450")
 frame.pack(side=tk.BOTTOM,padx = 10, pady = 15)
 view = tk.Label()
 view.pack()
-
-btn1 = tk.Button(frame, text = "Select Image to Restore", command=selectImage)
+obj = Restore(view)
+btn1 = tk.Button(frame, text = "Select Image to Restore", command=obj.selectImage)
 btn1.pack(side=tk.LEFT,padx=10)
 
-restore = tk.Button(frame,text="Restore Image",command=fixImage)
+restore = tk.Button(frame,text="Restore Image",command=obj.fixImage)
 restore.pack(side=tk.LEFT,padx=17)
 
 main.mainloop()
